8ddb47e5f8f958181a2db73832260c0d9403558a
Mateo Noguera Pinto <mateonoguerapinto@gmail.com>
2026-06-20 15:27:51 -0500

    refactor: Moved serializers, views and urls to api folder

--- apps/users/api/views.py
+++ apps/users/api/views.py
@@ -1,10 +1,9 @@
 from rest_framework.generics import CreateAPIView
 
-from .models import User
+from apps.users.models import User
 from .serializers import RegisterSerializer
 
 
 class RegisterView(CreateAPIView):
-
     queryset = User.objects.all()
     serializer_class = RegisterSerializer
